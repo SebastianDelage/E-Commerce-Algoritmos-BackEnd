7fce45f652e59a65b29ce9cb2d46858ee5b13d00
Hilet Aulas <hilet@HILET.LOCAL>
2025-10-09 08:23:58 -0300

    .

--- E-Commerce-Algoritmos-BackEnd-seba/E-commerce/E-commerce/obj/Debug/net8.0/E-commerce.AssemblyInfo.cs
+++ E-Commerce-Algoritmos-BackEnd-seba/E-commerce/E-commerce/obj/Debug/net8.0/E-commerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-commerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bab50c7d0d74b9bd88a82096e451d4ee38ed51a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61b25a3beea35e95202091bf313b53db0f634c5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-commerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-commerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
