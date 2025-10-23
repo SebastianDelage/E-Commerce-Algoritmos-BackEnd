a9d74d0526923b0658a402dd72df2b87effdc8e6
Hilet Aulas <hilet@HILET.LOCAL>
2025-10-23 09:57:11 -0300

    Promociones(model,handler,controller)

---
+++ E-Commerce-Algoritmos-BackEnd-seba/E-commerce/E-commerce/Endpoints/Promocion/Handlers/PromocionesQuery.cs
@@ -0,0 +1,15 @@
+﻿namespace E_commerce.Endpoints.Promocio.Handlers
+{
+    public class PromocionesQuery
+    {
+        public const string GetAllPromociones = "SELECT * FROM promociones";
+        public string GetPromocionesById(int id)
+        {
+            return string.Format($"SELECT * FROM promociones WHERE promociones_id = {id}");
+        }
+
+        public const string CreatePromocion = @"INSERT INTO promociones (nombre, tipo, valor, estado) VALUES (?,?,?,?);";
+        public const string UpdatePromocion = @"UPDATE promociones SET nombre = ?, tipo = ?, valor = ?,estado = ? ;";
+
+    }
+}
--- E-Commerce-Algoritmos-BackEnd-seba/E-commerce/E-commerce/obj/Debug/net8.0/E-commerce.AssemblyInfo.cs
+++ E-Commerce-Algoritmos-BackEnd-seba/E-commerce/E-commerce/obj/Debug/net8.0/E-commerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-commerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec791218a119e39b1e0c7534b66f1d278ad6d1a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+749bdb1e406e850543a31557e271ae07afb52cab")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-commerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-commerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
